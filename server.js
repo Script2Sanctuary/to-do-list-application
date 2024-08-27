b2d70d4b3b3408f12791ac4e335d9a4f8a717d42
aulawisaabiq <aulawisaabiq@gmail.com>
2024-08-27 15:03:34 +0700

    ADD: Login and Register

--- server.js
+++ server.js
@@ -1,92 +1,125 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const User = require('./models/user');
+const Task = require('./models/task');
 const app = express();
 
-// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-// Menghubungkan ke MongoDB menggunakan Mongoose tanpa opsi deprecated
-mongoose.connect('mongodb://127.0.0.1:27017/ToDoApp')
-    .then(() => {
-        console.log('Connected to MongoDB');
-        app.listen(3000, () => {
-            console.log('Server running on http://localhost:3000');
-        });
-    })
-    .catch(err => console.error('Failed to connect to MongoDB:', err));
+// Session setup
+app.use(session({
+    secret: 'secretKey',
+    resave: false,
+    saveUninitialized: true
+}));
 
-// Membuat skema dan model untuk tugas
-const taskSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    completed: { type: Boolean, default: false }
+// Connect to MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/ToDoApp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error(err));
+
+// Middleware for checking login status
+function checkAuth(req, res, next) {
+    if (req.session.userId) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+}
+
+// Routes
+app.get('/login', (req, res) => {
+    res.render('login');
 });
 
-const Task = mongoose.model('Task', taskSchema);
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (user && await user.comparePassword(password)) {
+        req.session.userId = user._id;
+        res.redirect('/');
+    } else {
+        res.redirect('/login');
+    }
+});
 
-// Rute untuk menampilkan semua tugas
-app.get('/', (req, res) => {
-    Task.find()
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const user = new User({ username, password });
+        await user.save();
+        res.redirect('/login');
+    } catch (err) {
+        res.redirect('/register');
+    }
+});
+
+app.get('/logout', (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/login');
+    });
+});
+
+app.get('/', checkAuth, (req, res) => {
+    Task.find({ userId: req.session.userId })
         .then(tasks => res.render('index', { tasks }))
-        .catch(err => {
-            console.error(err);
-            res.sendStatus(500);
-        });
+        .catch(err => res.sendStatus(500));
 });
 
-// Rute untuk menambahkan tugas baru
-app.post('/tasks', (req, res) => {
-    const newTask = new Task({ title: req.body.title });
+app.post('/tasks', checkAuth, (req, res) => {
+    const newTask = new Task({ title: req.body.title, userId: req.session.userId });
     newTask.save()
         .then(() => res.redirect('/'))
-        .catch(err => {
-            console.error(err);
-            res.sendStatus(500);
-        });
+        .catch(err => console.error(err));
 });
 
-// Rute untuk menampilkan form edit tugas
-app.get('/edit/:id', (req, res) => {
-    Task.findById(req.params.id)
+app.post('/tasks/complete/:id', checkAuth, (req, res) => {
+    Task.findOneAndUpdate(
+        { _id: req.params.id, userId: req.session.userId },
+        { completed: req.body.completed === 'true' }
+    )
+    .then(() => res.redirect('/'))
+    .catch(err => console.error(err));
+});
+
+app.post('/tasks/delete/:id', checkAuth, (req, res) => {
+    Task.findOneAndDelete({ _id: req.params.id, userId: req.session.userId })
+        .then(() => res.redirect('/'))
+        .catch(err => console.error(err));
+});
+
+app.get('/edit/:id', checkAuth, (req, res) => {
+    Task.findOne({ _id: req.params.id, userId: req.session.userId })
         .then(task => res.render('edit', { task }))
-        .catch(err => {
-            console.error(err);
-            res.sendStatus(500);
-        });
+        .catch(err => res.sendStatus(500));
 });
 
-// Rute untuk memperbarui tugas
-app.post('/tasks/:id', (req, res) => {
-    Task.findByIdAndUpdate(req.params.id, { title: req.body.title })
+app.post('/edit/:id', checkAuth, (req, res) => {
+    Task.findOneAndUpdate({ _id: req.params.id, userId: req.session.userId }, { title: req.body.title })
         .then(() => res.redirect('/'))
-        .catch(err => {
-            console.error(err);
-            res.sendStatus(500);
-        });
+        .catch(err => console.error(err));
 });
 
-// Rute untuk menghapus tugas
-app.post('/tasks/delete/:id', (req, res) => {
-    Task.findByIdAndDelete(req.params.id)
-        .then(() => res.redirect('/'))
-        .catch(err => {
-            console.error(err);
-            res.sendStatus(500);
-        });
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
 });
 
-// Rute untuk menandai tugas sebagai selesai atau tidak selesai
-app.post('/tasks/complete/:id', (req, res) => {
-    Task.findById(req.params.id)
-        .then(task => {
-            // Toggle status completed
-            task.completed = !task.completed;
-            return task.save();
-        })
-        .then(() => res.redirect('/'))
-        .catch(err => {
+// Rute untuk logout
+app.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
             console.error(err);
-            res.sendStatus(500);
-        });
+            return res.redirect('/');
+        }
+        res.redirect('/login');
+    });
 });
